8800c021caaa980ce86b70101dba33712bbc6eaa
Viktor Blazhevski <victordeflos@hotmail.com>
2021-04-05 23:05:50 +0200

    initial Repos with icon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i> Repos
+      </h1>
      
     </div>
   );
